f0286a84596fb01dede5d087c01bdc742c8d28d9
ergenekonyigit <ergenekonyigit@hotmail.com>
2016-02-20 02:44:56 +0200

    Python Language Examples Added

---
+++ Python/newtonmethod.py
@@ -0,0 +1,21 @@
+import math
+
+i = 0
+eps = 1e-6
+x0 = input("x0: ")
+
+def f(x):
+    return x*math.exp(-x)+math.pow(x, 3.0)+1
+
+def Df(x):
+    return math.exp(-x)-x*math.exp(-x)+3*math.pow(x, 2.0)
+
+print('i', "  x0", "  f(x0)", "  Df(x0)", "  x1", "  Dx")
+while True:
+    x1 = x0-f(x0)/Df(x0)
+    Dx = math.fabs(x1-x0)
+    print(i+1, "%.4f" % x0, "%.4f" % f(x0), "%.4f" % Df(x0), "%.4f" % x1, "%.4f" % Dx)
+    i+=1
+    x0 = x1
+    if Dx < eps:
+        break
---
+++ Python/regulafalsimethod.py
@@ -0,0 +1,20 @@
+import math
+
+i = 0
+eps = 1e-6
+a = input("a: ")
+b = input("b: ")
+
+def f(x):
+    return 10*math.exp(-x/2.0)*(math.cos(6.0*x)+math.sin(8.0*x))
+
+print('i', "  a", "  b", "   f(a)", "   f(b)", "   c", "   f(c)", " +/-")
+while True:
+    c = (a*f(b)-b*f(a))/(f(b)-f(a))
+    s = "+" if f(c) > 0 else "-"
+    print(i+1, "%.4f" % a, "%.4f" % b, "%.4f" % f(a), "%.4f" % f(b), "%.4f" % c, "%.4f" % f(c), s)
+    i+=1
+    if f(a)*f(c) > 0: a = c
+    else: b = c
+    if math.fabs(f(c)) < eps:
+        break
