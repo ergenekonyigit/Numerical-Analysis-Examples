309053262f65af507eb177218e0f7d72b4335e12
Taha Ibrahim Bayram <lacriment@gmail.com>
2016-05-15 17:29:19 +0300

    Recursive implementation of Newton's Method in Racket

---
+++ Racket (Scheme) /newtons-method.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define EPS 1e-3)
+
+(define (newtons-method f df x0 n)
+  (define x1 (- x0 (/ (f x0) (df x0))))
+  (define dx (abs (- x1 x0)))
+  (if (>= dx EPS)
+      (newtons-method f df x1 (+ 1 n))
+      x1))
+
+(newtons-method (λ (x) (+ (* x (exp (* -1 x))) (expt x 3) 1))
+                (λ (x) (+ (* (- (exp (* -1 x)) x) (exp (* -1 x))) (* 3 (expt x 2))))
+                0
+                0)
