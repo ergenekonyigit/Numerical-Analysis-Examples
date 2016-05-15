983954b6b4b2fa2092fa6eaef77f8de905485b5e
Konstantine Mushegian <kmushegi@bowdoin.edu>
2016-05-15 15:54:24 -0400

    added C++ implementation of the bisection method

---
+++ C++/bisectionmethod.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+
+/* 
+Usage: ./a.out a b n-steps
+*/
+
+double a, b, stepcount;
+
+std::vector<double> list;
+
+double f(double x) {
+	return x * exp(-x) + pow(x, 3.0) + 1;
+}
+
+int main(int argc, char *argv[]) {
+	if(argc != 4) {
+		std::cout<<"Incorrect Usage. See .cpp file for usage information\n";
+		exit(0);
+	} else {
+		a = atof(argv[1]);
+		b = atof(argv[2]);
+		stepcount = atof(argv[3]);
+	}
+	list.push_back(a);
+
+	for(int i=1; i <= stepcount; ++i) {
+		double mid = (a+b)/2;
+		list.push_back(mid);
+		if(f(mid) * f(a) <= 0) {
+			b = mid;
+		} else {
+			a = mid;
+		}
+	}
+
+	for(int i=0; i < list.size(); ++i) {
+		std::cout<<list[i]<<std::endl;;
+	}
+	return 0;
+}
