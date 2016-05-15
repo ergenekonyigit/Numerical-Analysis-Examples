a96af8223e7d3923472e3a989b0e3023c711313b
Taha Ibrahim Bayram <lacriment@gmail.com>
2016-05-15 15:44:44 +0300

    non-opposite signature condition added to bisection.py

--- Python/bisectionmethod.py
+++ Python/bisectionmethod.py
@@ -9,16 +9,19 @@ b = input("b: ")
 def f(x):
     return x*math.exp(-x)+math.pow(x, 3.0)+1
 
-print("n", "  a", "  b", "    m", "    f(a)*f(m)", "|Dx/2|")
 
-while True:
-    m = (a + b) / 2.0
-    Dx = math.fabs((b - a) / 2.0)
-    print(n+1, "%.4f" % a, "%.4f" % b, "%.4f" % m, "+" if f(a) * f(m) > 0 else "-", "%.4f" % Dx)
-    if f(a) * f(m) < 0:
-        b = m
-    else:
-        a = m
-    n += 1
-    if Dx < eps:
-        break
+if f(a) * f(b) > 0:
+    print "No root: The function values at the initial state should be opposite sign."
+else:
+    print("n", "  a", "  b", "    m", "    f(a)*f(m)", "|Dx/2|")
+    while True:
+        m = (a + b) / 2.0
+        Dx = math.fabs((b - a) / 2.0)
+        print(n+1, "%.4f" % a, "%.4f" % b, "%.4f" % m, "+" if f(a) * f(m) > 0 else "-", "%.4f" % Dx)
+        if f(a) * f(m) < 0:
+            b = m
+        else:
+            a = m
+        n += 1
+        if Dx < eps:
+            break
