dacd0d102a3d3d3097f5e724fc62575968048465
scott <scott@scottjjohnson.com>
2016-05-16 15:16:36 -0400

    Switching to MIT license to be consistent with the rest of the project

--- Python/lagrangeinterpolation.py
+++ Python/lagrangeinterpolation.py
@@ -1,20 +1,28 @@
 #
-# Copyright 2016 Scott J. Johnson (http://scottjjohnson.com)
+# The MIT License (MIT)
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Copyright (c) 2016 Scott J. Johnson
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS-IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
 
-def lagrangepolynomialinterpolation(x, xp, yp):
+def lagrangeinterpolation(x, xp, yp):
     '''
     Example implementation of the Lagrange Polynomial Interpolation method.
 
@@ -52,8 +60,9 @@ x = 1.5
 xp = (1.0, 2.0, 3.0)
 yp = (1.0, 4.0, 9.0)
 
-print "Example #1 : ", zip(xp, yp)
-print "interpolated value at %.10f is %.10f" % (x, lagrangepolynomialinterpolation(x, xp, yp))
+print "\nExample #1: "
+print "Given points: ", zip(xp, yp)
+print "interpolated y value = %.3f at x = %.3f" % (lagrangeinterpolation(x, xp, yp), x)
 
 # example #2: x**3
 
@@ -61,6 +70,7 @@ x = 2.5
 xp = (1.0, 2.0, 3.0)
 yp = (1.0, 8.0, 27.0)
 
-print "Example #2 : ", zip(xp, yp)
-print "interpolated value at %.10f is %.10f" % (x, lagrangepolynomialinterpolation(x, xp, yp))
+print "\nExample #2: "
+print "Given points: ", zip(xp, yp)
+print "interpolated y value = %.3f at x = %.3f" % (lagrangeinterpolation(x, xp, yp), x)
 
