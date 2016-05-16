8a9ea7a746a893315e45bbe4e20ea511df602671
ergenekonyigit <ergenekonyigit@hotmail.com>
2016-05-16 18:09:09 +0300

    Few methods replaced with console app

---
+++ C#/GaussianEliminationMethod/gaussianeliminationmethod.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace GaussianEliminationMethod
+{
+	class MainClass
+	{
+		
+		public static void Main (string[] args)
+		{
+			int m = Convert.ToInt32 (Console.ReadLine ());
+			Random r = new Random ();
+
+			double[] x1 = new double[m];
+			for (int i = 0; i < m; i++) {
+				x1 [i] = Math.Round (r.NextDouble () * 200 - 100);
+			}
+			double[,] a = new double[m, m];
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++) {
+					a [i, j] = Math.Round (r.NextDouble () * 100 - 50);
+				}
+			}
+			double[] b = new double[m];
+			for (int i = 0; i < m; i++) {
+				double t = 0;
+				for (int j = 0; j < m; j++) {
+					t += a [i, j] * x1 [j];
+				}
+				b [i] = t;
+			}
+			print(a, x1, b);
+			double[] x2 = GaussianElimination (a, b);
+			Console.ReadLine ();
+		}
+
+		private static double[] GaussianElimination(double[,] a, double[] b)
+		{
+			int m = b.GetLength(0);
+
+			double[] x = new double[m];
+			double p;
+			for (int k = 0; k < m - 1; k++) {
+				for (int i = k + 1; i < m; i++) {
+					p = a [i, k] / a [k, k];
+					for (int j = k; j < m; j++) {
+						a [i, j] -= p * a [k, j];
+					}
+					b [i] -= p * b [k];
+				}
+			}
+			for (int i = m - 1; i >= 0; i--) {
+				double t = b [i];
+				for (int j = i + 1; j < m; j++) {
+					t -= a [i, j] * x [j];
+				}
+				x [i] = t / a [i, i];
+			}
+			print(a, x, b);
+			return x;
+		}
+
+		private static void print(double[,] a, double[] x, double[] b)
+		{
+			int count = 0;
+			int m = b.GetLength(0);
+			double[,] w = new double[10 * m, 10 * m]; 
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++) {
+					w [j, i + count] = a [i, j];
+				}
+				w [m + 1, i + count] = x [i];
+				w [m + 3, i + count] = b [i];
+			}
+
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++) {
+					Console.Write (string.Format("{0}\t",w[i,j]));
+				}
+				Console.Write (Environment.NewLine + Environment.NewLine);
+			}
+
+			count += m + 1;
+		}
+	}
+}
---
+++ C#/InverseMatrixwithCramersRule/inversematrixcramersrule.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace InverseMatrixwithCramersRule
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int m = Convert.ToInt32 (Console.ReadLine ());
+			Random r = new Random ();
+			double[,] a = new double[m, m];
+			double[,] R = new double[m, m];
+
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++) {
+					a [i, j] = Math.Round (r.NextDouble () * 20 - 10);
+				}
+			}
+
+			print (a);
+			double D = determinant (a);
+
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++) {
+					R [j, i] = Minor (a, i, j) / D;
+				}
+			}
+			print (R);
+			Console.ReadLine ();
+		}
+
+		private static double Minor(double[,] a, int row, int col)
+		{
+			int m = a.GetLength(0);
+			double[,] M = new double[m - 1, m - 1];
+			int ii = 0, jj = 0;
+
+			for (int i = 0; i < m; i++) {
+				if (i != row) {
+					jj = 0;
+					for (int j = 0; j < m; j++) {
+						if (j != col) {
+							M [ii, jj] = a [i, j];
+							jj++;
+						}
+					}
+					ii++;
+				}
+			}
+			return determinant(M);
+		}
+
+		private static double determinant(double[,] a)
+		{
+			int m = a.GetLength (0);
+			if (m == 1) {
+				return a [0, 0];
+			}
+			double[,] c = new double[m - 1, m - 1];
+			double t = 0;
+			int n;
+			for (int j = 0; j < m; j++) {
+				for (int i = 1; i < m; i++) {
+					n = 0;
+					for (int k = 0; k < m; k++) {
+						if (j != k) {
+							c [i - 1, n++] = a [i, k];
+						}
+					}
+				}
+				t += Math.Pow (-1, j) * a [0, j] * determinant (c);
+			}
+			return t;
+		}
+
+		private static void print(double[,] c)
+		{
+			int count = 0;
+			int m = c.GetLength (0);
+			double[,] w = new double[10 * m, 10 * m];
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++) {
+					w [j, i + count] = c [i, j];
+				}
+			}
+
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < m; j++) {
+					Console.Write (string.Format ("{0}\t", w [i, j]));
+				}
+				Console.Write (Environment.NewLine + Environment.NewLine);
+			}
+
+			count += m + 1;
+		}
+	}
+}
